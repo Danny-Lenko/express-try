84dcdab9147b7f3d2770408285dcf85895e742b4
Valerii Danylenko <devdanny.14@gmail.com>
2023-06-06 12:06:08 +0300

    sent the postman requests

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-// const express = require('express')
 import express from 'express';
+import bodyParser from 'body-parser';
+
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World')
+})
+
+app.get('/profile', (req, res) => {
+   res.send('Hello profile')
+})
+
+app.post('/profile', (req, res) => {
+   console.log(req.body)
+   res.send('Success')
 })
 
 app.listen(port, () => {
